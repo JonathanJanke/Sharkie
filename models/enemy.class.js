df759e40e241a8c514f2e65384c1538ef9250a02
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-04-29 16:50:51 +0200

    new: try to add interval for moving backwards @enemy.class.js

--- models/enemy.class.js
+++ models/enemy.class.js
@@ -1,10 +1,16 @@
 class squid extends MoveableObject{
-    
+    y = 400;
     SWIM_IMGS = [
         './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png',
         './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 2.png',
         './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 3.png',
         './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png',
+    ]
+    SWIM_IMGS_BW = [
+        './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png',
+        './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 3.png',
+        './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 2.png',
+        './assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png',
     ]
 
     constructor () {
@@ -15,13 +21,25 @@ class squid extends MoveableObject{
         this.animate();
     }
     animate () {
-        setInterval(() => {
+        let startFW = setInterval(() => {
             this.y -= 4;
             
             let i = this.currentImg % this.SWIM_IMGS.length;
             let path = this.SWIM_IMGS[i];
             this.img = this.imgCache[path];
             this.currentImg++;
+            if(this.y < 0){clearInterval(start); startBW = setInterval(() => {
+                this.y += 4;
+                let i = this.currentImg % this.SWIM_IMGS_BW.length;
+                let path = this.SWIM_IMGS_BW[i];
+                this.img = this.imgCache[path];
+                this.currentImg++;
+                if(this.y > 399){
+                    clearInterval(startBW);
+                    this.y = 400;
+                    this.animate();
+                }
+            }, 200/0.75)};
         }, 200/0.75);
     }
 }
